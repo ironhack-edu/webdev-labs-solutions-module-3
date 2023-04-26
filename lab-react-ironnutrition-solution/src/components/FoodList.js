faee902ef009d8a383829fea352ffae316390845
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2023-04-27 01:25:37 +0200

    feat: update solution after migration to vite & vitest

--- lab-react-ironnutrition-solution/src/components/FoodList.js
+++ lab-react-ironnutrition-solution/src/components/FoodList.js
@@ -1,24 +1,27 @@
-import foodsJSON from './../foods.json';
-import { useState } from 'react';
-import FoodBox from './../components/FoodBox';
-import AddFoodForm from './../components/AddFoodForm';
+import foodsJSON from "./../foods.json";
+import { useState } from "react";
+import FoodBox from "./../components/FoodBox";
+import AddFoodForm from "./AddFoodForm";
 import EmptyList from './EmptyList';
 import Search from './Search';
-import { Button } from 'antd';
-import { v4 as randomId } from 'uuid';
 
 function FoodList() {
   const [foods, setFoods] = useState(foodsJSON); // Array that we render/show as a list
   const [updatedFoods, setUpdatedFoods] = useState(foods);
-  const [formShowing, setFormShowing] = useState(false);
-
-  const toggleForm = () => setFormShowing(!formShowing);
 
   const addFood = (foodObj) => {
     const updatedFoods = [foodObj, ...foods];
 
     setFoods(updatedFoods);
     setUpdatedFoods(updatedFoods);
+  };
+
+  const deleteFood = (foodNameStr) => {
+    const filteredFoods = updatedFoods.filter((oneFood) => {
+      return oneFood.name !== foodNameStr; // exclude the food object we want to delete
+    });
+
+    setUpdatedFoods(filteredFoods);
   };
 
   const filterFoodList = (char) => {
@@ -32,31 +35,18 @@ function FoodList() {
       });
     }
     setUpdatedFoods(filteredFoods);
-  };
-
-  const deleteFood = (foodNameStr) => {
-    const filteredFoods = updatedFoods.filter((oneFood) => {
-      return oneFood.name !== foodNameStr; // exclude the food object we want to delete
-    });
-
-    setUpdatedFoods(filteredFoods);
-  };
+  };  
 
   return (
     <div className="App">
-      <Button onClick={toggleForm} className="toggle-btn">
-        {formShowing ? 'Hide Form' : 'Add New Food'}
-      </Button>
-      {formShowing && <AddFoodForm addFoodHandler={addFood} />}
-
-      {/* <AddFoodForm addFood={addFood} /> */}
+      <AddFoodForm addFoodHandler={addFood} />
 
       <Search filterFoodHandler={filterFoodList} />
       <div className="foods-list">
         {!updatedFoods.length && <EmptyList />}
 
         {updatedFoods.map((oneFood) => (
-          <FoodBox food={oneFood} deleteFood={deleteFood} key={randomId()} />
+          <FoodBox food={oneFood} deleteFood={deleteFood} key={oneFood.id} />
         ))}
       </div>
     </div>
